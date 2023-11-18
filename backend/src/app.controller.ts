b62843282a919d2d528b98380b2f7ee58fe78604
Andre Santos <andrefelipe_santos@outlook.com>
2023-11-18 15:14:00 -0300

    Recebendo o upload do arquivo.

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, HttpStatus, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import { Response } from 'express'
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  @UseInterceptors(FileInterceptor('file', {dest: 'uploads/'}))
+  async uploadFile(@UploadedFile() file: Express.Multer.File, @Res() res: Response) {
+    const data = this.appService.getData(file)
+
+    res.status(HttpStatus.CREATED).json({
+      message: "Arquivo enviado"
+    })
   }
 }
