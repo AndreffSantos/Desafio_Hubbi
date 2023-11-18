45602a88b51c2a5f87723f5e743e2e4cdca7c57b
Andre Santos <andrefelipe_santos@outlook.com>
2023-11-18 15:14:23 -0300

    tratamento dos dados do arquivo.

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs'
+import { Transaction } from './entities/entities';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getData(file: Express.Multer.File): Transaction[] {
+    let transaction = new Array()
+    const linesFile = fs.readFileSync(`uploads/${file.filename}`, 'utf-8').split('\r\n')
+
+    fs.rm(`uploads/${file.filename}`, (err) => {if(err) console.log(err.message)})
+    
+    for(let line in linesFile){
+      transaction.push({
+        tipo: Number(linesFile[line].split('').splice(0,1).join('')),
+        data: new Date(linesFile[line].split('').splice(1,24).join('')),
+        produto: linesFile[line].split('').splice(25,30).join(''),
+        valor: Number(linesFile[line].split('').splice(55,10).join('')),
+        vendedor: linesFile[line].split('').splice(65,20).join(''),
+      })
+    }
+    return transaction;
   }
 }
---
+++ backend/src/entities/entities.ts
@@ -0,0 +1,7 @@
+export interface Transaction{
+    tipo: number,
+    data: Date,
+    produto: string,
+    valor: number,
+    vendedor: string,
+}
